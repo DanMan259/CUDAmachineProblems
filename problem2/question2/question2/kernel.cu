87d60c8162953e3434271d49792d82dd719e9854
Daniyal Maniar <16mdm13@queensu.ca>
2020-03-26 00:00:21 -0400

    Started machine problem 3

--- problem2/question2/question2/kernel.cu
+++ problem2/question2/question2/kernel.cu
@@ -43,94 +43,118 @@ void sumMatrixCPU(float* A, float* B, float* C, const int N){
 		C[i] = A[i] + B[i];
 }
 
-void checkResult(float* hostRef, float* gpuRef, const int N) {
+void checkResult(float* CPU, float* GPU, const int N) {
 	double epsilon = 1.0E-8;
+	
 	for (int i = 0; i < (N*N); i++){
-		if (abs(hostRef[i] - gpuRef[i]) > epsilon){
-			printf("host %f gpu %f ", hostRef[i], gpuRef[i]);
+		if (abs(CPU[i] - GPU[i]) > epsilon){
+			printf("CPU %f GPU %f ", CPU[i], GPU[i]);
 			printf("Arrays do not match.\n\n");
-			break;
+			return;
 		}
 	}
 	printf("Test PASSED\n\n");
 }
 
-void computeMatrix(int N) {
+void printArr(float* matrix, const int N) {
+	printf("[");
+	for (int i = 0; i < (N*N); i++)
+		printf("%f,", matrix[i]);
+	printf("\b]\n");
+
+}
+
+void computeMatrix(const int N) {
+	// Initial prints
+	printf("------------------------------------------------------------------------\n\n");
+	printf("%dx%d matrix addition.\n\n", N, N);
+
+	// Initialize Host variables
 	float* C_A, *C_B, *C_C, *C_C1;
 	size_t size = N * N * sizeof(float);
 	
+	// Initialize space
 	C_A = (float*)malloc(size);
 	C_B = (float*)malloc(size);
 	C_C = (float*)malloc(size);
 	C_C1 = (float*)malloc(size);
-	
+
+	// Set with random data
 	initialData(C_A, N);
 	initialData(C_B, N);
 	memset(C_C, 0, N);
 	memset(C_C1, 0, N);
-	
-	auto start = std::chrono::higC_Cesolution_clock::now();
-	sumMatrixCPU(C_A, C_B, C_C, N);
-	auto end = std::chrono::higC_Cesolution_clock::now();
-	auto timeElapse = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	printf("The CPU took %d to perform the computation.\n\n", timeElapse);
-	
+
+	// Initialize GPU variables
 	float* G_A, *G_B, *G_C, *G_C1, *G_C2;
-	
 	cudaMalloc((void**)&G_A, size);
 	cudaMalloc((void**)&G_B, size);
 	cudaMalloc((void**)&G_C, size);
 	cudaMalloc((void**)&G_C1, size);
 	cudaMalloc((void**)&G_C2, size);
 	
+	// Copy over the data
 	cudaMemcpy(G_A, C_A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(G_B, C_B, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(G_B, C_B, size, cudaMemcpyHostToDevice);
+
+	// Serial Test CPU
+	auto start = std::chrono::high_resolution_clock::now();
+	sumMatrixCPU(C_A, C_B, C_C, N);
+	auto end = std::chrono::high_resolution_clock::now();
+	auto timeElapse = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+	printf("The CPU took %d to perform the computation.\n\n", timeElapse);
 	
+	
+	// Test Complete parallel Computation
 	dim3 block(16, 16);
 	dim3 thread((N + block.x - 1) / block.x, (N + block.y - 1) / block.y);
 	
-	start = std::chrono::higC_Cesolution_clock::now();
+	start = std::chrono::high_resolution_clock::now();
 	cudaDeviceSynchronize();
-	sumMatrixGPU <<<thread, block >>> (G_A, G_B, G_C, N, N);
+	sumMatrixGPU <<<thread, block >>> (G_A, G_B, G_C, N);
 	cudaDeviceSynchronize();
-	end = std::chrono::higC_Cesolution_clock::now();
+	end = std::chrono::high_resolution_clock::now();
 	timeElapse = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	printf("The GPU took %d to perform the computation with one thread per element.\n\n", timeElapse);
+	printf("The GPU took %d to perform the computation with one thread per element.\n", timeElapse);
 	
+	// Copy over the result and compare
 	cudaMemcpy(C_C1, G_C, size, cudaMemcpyDeviceToHost);
-	checkResult(C_C, C_C1, N * N);
+	checkResult(C_C, C_C1, N);
 	
-	
+	// Test row based parallel Computation
 	dim3 block1(16);
 	dim3 thread1((N + block1.x - 1) / block1.x);
 	
-	start = std::chrono::higC_Cesolution_clock::now();
+	start = std::chrono::high_resolution_clock::now();
 	cudaDeviceSynchronize();
 	sumMatrixGPUperRow <<<thread1, block1 >>> (G_A, G_B, G_C1, N);
 	cudaDeviceSynchronize();
-	end = std::chrono::higC_Cesolution_clock::now();
+	end = std::chrono::high_resolution_clock::now();
 	timeElapse = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	printf("The GPU took %d to perform the computation with one thread per Row.\n\n", timeElapse);
-	
+	printf("The GPU took %d to perform the computation with one thread per Row.\n", timeElapse);
+
+	// Copy over the result and compare
 	cudaMemcpy(C_C1, G_C1, size, cudaMemcpyDeviceToHost);
-	checkResult(C_C, C_C1, N * N);
-	
+	checkResult(C_C, C_C1, N);
+
+	// Test Complete parallel Computation
 	dim3 block2(16);
 	dim3 thread2((N + block2.x - 1) / block2.x);
 	
-	
-	start = std::chrono::higC_Cesolution_clock::now();
+	// Test column based parallel Computation
+	start = std::chrono::high_resolution_clock::now();
 	cudaDeviceSynchronize();
 	sumMatrixGPUperCol <<<thread2, block2 >>> (G_A, G_B, G_C2, N);
 	cudaDeviceSynchronize();
-	end = std::chrono::higC_Cesolution_clock::now();
+	end = std::chrono::high_resolution_clock::now();
 	timeElapse = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	printf("The GPU took %d to perform the computation with one thread per Column.\n\n", timeElapse);
+	printf("The GPU took %d to perform the computation with one thread per Column.\n", timeElapse);
 	
+	// Copy over the result and compare
 	cudaMemcpy(C_C1, G_C2, size, cudaMemcpyDeviceToHost);
-	checkResult(C_C, C_C1, N * N);
+	checkResult(C_C, C_C1, N);
 	
-	
+	// Free all the memory
 	cudaFree(G_A);
 	cudaFree(G_B);
 	cudaFree(G_C);
@@ -144,22 +168,13 @@ void computeMatrix(int N) {
 }
 
 int main(){
-    printf("100x100 matrix addition.\n");
 	computeMatrix(100);
-	
-	//printf("200x200 matrix addition.\n");
-	//computeMatrix(200);
-	
-	//printf("500x500 matrix addition.\n");
-	//computeMatrix(500);
-	
-	//printf("1000x1000 matrix addition.\n");
-	//computeMatrix(1000);
-	
-	//printf("1500x1500 matrix addition.\n");
-	//computeMatrix(1500);
-	
-	//printf("5000x5000 matrix addition.\n");
-	//computeMatrix(14200);
+	computeMatrix(200);
+	computeMatrix(500);
+	computeMatrix(1000);
+	computeMatrix(1500);
+	computeMatrix(3000);
+	computeMatrix(5000);
+	printf("------------------------------------------------------------------------\n\n");
     return 0;
 }
